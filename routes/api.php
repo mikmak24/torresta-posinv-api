bf0829e44961f7e023567eacde1ed2622e8297ce
mikmak24 <markedwardpanabe@gmail.com>
2023-04-17 18:55:11 +0800

    Fixed

--- routes/api.php
+++ routes/api.php
@@ -28,33 +28,10 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group( function () {
 
-    //Categories
-    Route::group(['prefix' => 'category'], function () {
-        Route::get('', [CategoryController::class, 'index']); //Retrieved all categories
-        Route::get('find/{id}', [CategoryController::class, 'show']); //Retrieved specific categories
-        Route::post('store', [CategoryController::class, 'store']); //Create New Category
-        Route::put('{id}', [CategoryController::class, 'update']); //Update Specific Category
-        Route::delete('{id}', [CategoryController::class, 'destroy']); //Update Specific Category
-    });
+    Route::resource('category', CategoryController::class);
 
-    //Products
-    Route::group(['prefix' => 'product'], function () {
-        Route::get('', [ProductController::class, 'index']); //Retrieved all Products
-        Route::get('find/{id}', [ProductController::class, 'show']); //Retrieved specific Product
-        Route::post('store', [ProductController::class, 'store']); //Create New Product
-        Route::put('{id}', [ProductController::class, 'update']); //Update Specific Product
-        Route::delete('{id}', [ProductController::class, 'destroy']); //Delete Specific Product
-    });
+    Route::resource('product', ProductController::class);
 
-    //orders
-    Route::group(['prefix' => 'order'], function () {
-        Route::get('', [OrderController::class, 'index']); //Retrieved all Products
-        Route::get('find/{id}', [OrderController::class, 'show']); //Retrieved specific Product
-        Route::post('store', [OrderController::class, 'store']); //Create New Product
-        // Route::put('{id}', [ProductController::class, 'update']); //Update Specific Product
-        // Route::delete('{id}', [ProductController::class, 'destroy']); //Delete Specific Product
-    });   
-
-
+    Route::resource('order', OrderController::class);
 
 });
